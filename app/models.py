aa4e7934b8b79048a153174276912348f6f317e7
slocksert <rsdlj01@gmail.com>
2025-02-11 21:02:57 -0300

    Refactor user model to include name field

--- app/models.py
+++ app/models.py
@@ -1,21 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
 
-class UserManager(BaseUserManager):
-    def create_user(self, username, email, sector, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, sector=sector, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, sector, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        return self.create_user(username, email, sector, password, **extra_fields)
 
 class User(AbstractBaseUser):
     name = models.CharField(max_length=100)
@@ -27,10 +12,8 @@ class User(AbstractBaseUser):
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(null=True, blank=True)
 
-    objects = UserManager()
-
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['username', 'email', 'sector']
+    REQUIRED_FIELDS = ['username', 'email', 'sector', 'name']
 
     def __str__(self):
         return self.username
