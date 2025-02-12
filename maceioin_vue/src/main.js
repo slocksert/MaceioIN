57e33ee348953699df7190c7bfcadb906e0c8297
slocksert <rsdlj01@gmail.com>
2025-02-12 10:43:44 -0300

    Refactor main.js to use Vue Router for routing

--- maceioin_vue/src/main.js
+++ maceioin_vue/src/main.js
@@ -1,4 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import LandingPage from './views/LandingPage.vue';
+import UserLogin from './views/UserLogin.vue';
+import UserRegister from './views/UserRegister.vue';
+import UserManagement from './views/UserManagement.vue';
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: LandingPage },
+  { path: '/login', component: UserLogin },
+  { path: '/register', component: UserRegister },
+  { path: '/users', component: UserManagement }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
