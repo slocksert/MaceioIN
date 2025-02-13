5ec0dfb56b5f92f95794a63ff938546d4959390b
slocksert <rsdlj01@gmail.com>
2025-02-13 19:59:50 -0300

    Add UserManagementHeader component for displaying the header with logo and logout button

---
+++ maceioin_vue/src/components/UserManagementHeader.vue
@@ -0,0 +1,51 @@
+<template>
+    <header class="header">
+      <router-link to="/">
+        <img src="../assets/logo.png" alt="Maceió IN" />
+      </router-link>
+      <button @click="logout"> Logout </button>
+    </header>
+  </template>
+  
+<script>
+  export default {
+    name: 'PageHeader',
+    methods: {
+      logout() {
+        localStorage.removeItem('access_token');
+        this.$router.push('/login');
+      }
+    }
+  };
+</script>
+  
+<style scoped>
+  .header {
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+  }
+  
+  .header img {
+    max-width: 300px;
+  }
+  
+  .header h1 {
+    margin: 0;
+    color: #fff;
+  }
+
+  button {
+    border: none; 
+    border-radius: 4px; 
+    cursor: pointer;
+    color: white;
+    padding: 1rem;
+    background-color: #FF4136;
+    text-transform: capitalize;
+  }
+
+  button:hover {
+    background-color: #CC322A;
+  }
+</style>
