cb93d2d3320ed7be85b2ab9aae3cad25c1424371
slocksert <rsdlj01@gmail.com>
2025-02-13 19:59:33 -0300

    Add PageHeader component for displaying the header with logo and navigation link

---
+++ maceioin_vue/src/components/PageHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header">
+    <router-link to="/">
+      <img src="../assets/logo.png" alt="Maceió IN" />
+    </router-link>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'PageHeader'
+};
+</script>
+
+<style scoped>
+.header {
+  padding: 5px;
+  text-align: center;
+  background: linear-gradient(to right, #ff7f00, #007BFF);
+}
+
+.header img {
+  max-width: 300px;
+}
+
+.header h1 {
+  margin: 0;
+  color: #fff;
+}
+</style>
