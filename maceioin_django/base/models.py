f5c1ffabf7984deb292b93424a76b66c336ea75f
slocksert <rsdlj01@gmail.com>
2025-02-13 19:58:21 -0300

    Refactor User model in models.py

--- maceioin_django/base/models.py
+++ maceioin_django/base/models.py
@@ -7,9 +7,7 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     sector = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
-    last_login = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.username
