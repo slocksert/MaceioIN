2addd4d4a00df790d42b5cb3fce2e2fb1b0bc7ec
slocksert <rsdlj01@gmail.com>
2025-02-13 19:58:16 -0300

    Add token-based authentication and user login functionality

--- maceioin_django/api/views.py
+++ maceioin_django/api/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth import authenticate, login
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework.permissions import IsAdminUser
-from rest_framework.decorators import permission_classes
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from base.models import User
 from .serializers import UserSerializer
@@ -23,14 +24,13 @@ def add_user(request):
         return Response(serializer.errors, status=400)
     return Response(status=405)
 
-@api_view(['PUT'])
-@permission_classes([IsAdminUser])
+@api_view(['PATCH'])
 def edit_user(request, user_id):
     try:
         user = User.objects.get(id=user_id)
     except User.DoesNotExist:
         return Response({'error': 'User not found'}, status=404)
-
+    
     serializer = UserSerializer(user, data=request.data, partial=True)
     if serializer.is_valid():
         serializer.save()
@@ -38,7 +38,6 @@ def edit_user(request, user_id):
     return Response(serializer.errors, status=400)
 
 @api_view(['DELETE'])
-@permission_classes([IsAdminUser])
 def delete_user(request, user_id):
     try:
         user = User.objects.get(id=user_id)
@@ -47,3 +46,18 @@ def delete_user(request, user_id):
 
     user.delete()
     return Response({'message': 'User deleted successfully'}, status=204)
+
+@api_view(['POST'])
+def login_user(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }, status=200)
+    else:
+        return Response({'error': 'Invalid credentials'}, status=400)
