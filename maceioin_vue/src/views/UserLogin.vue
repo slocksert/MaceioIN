6652d032849e85336ce9e5aa9c08142af4986e7c
slocksert <rsdlj01@gmail.com>
2025-02-13 20:00:28 -0300

    Refactor UserLogin.vue to use modular components and update styling

--- maceioin_vue/src/views/UserLogin.vue
+++ maceioin_vue/src/views/UserLogin.vue
@@ -1,78 +1,46 @@
 <template>
-  <div class="login">
-    <h2>Login</h2>
-    <form @submit.prevent="login">
-      <div>
-        <label for="username">Username:</label>
-        <input type="text" id="username" v-model="username" required />
-      </div>
-      <div>
-        <label for="password">Password:</label>
-        <input type="password" id="password" v-model="password" required />
-      </div>
-      <button type="submit">Login</button>
-    </form>
+  <div id="app">
+    <LoginForm />
+    <PageFooter />
   </div>
 </template>
 
 <script>
+import LoginForm from '../components/LoginForm.vue';
+import PageFooter from '../components/PageFooter.vue';
+
+
 export default {
-  name: 'UserLogin',
-  data() {
-    return {
-      username: '',
-      password: ''
-    };
-  },
-  methods: {
-    login() {
-      // Implement login logic here
-      this.$router.push('/users');
-    }
+  components: {
+    LoginForm,
+    PageFooter
   }
 };
 </script>
 
-<style scoped>
-.login {
-  max-width: 300px;
-  margin: 50px auto;
-  padding: 20px;
-  background-color: #fff;
-  border-radius: 8px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+<style>
+html, body, #app {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
 }
 
-.login h2 {
-  margin-bottom: 20px;
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; 
 }
 
-.login form div {
-  margin-bottom: 10px;
+main {
+  flex: 1;
 }
 
-.login label {
-  display: block;
-  margin-bottom: 5px;
-}
-
-.login input {
-  width: 100%;
-  padding: 8px;
-  box-sizing: border-box;
-}
-
-.login button {
-  width: 100%;
+.footer {
+  background: rgba(0, 0, 0, 0.781);
+  backdrop-filter: blur(10px);
   padding: 10px;
-  background-color: #007BFF;
+  text-align: center;
   color: #fff;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-.login button:hover {
-  background-color: #0056b3;
 }
 </style>
